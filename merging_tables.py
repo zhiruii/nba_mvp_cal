446f4831e044d1f68a2e5fe4495185125455d289
zhiruii <lizhirui0@gmail.com>
2025-06-28 13:20:48 +0800

    merging_tables.py

--- merging_tables.py
+++ merging_tables.py
@@ -1,5 +1,3 @@
-from importlib import invalidate_caches
-
 from scrapping_reg import reg_stats_table
 from scraping import advanced_stats_table
 from historical_MVP_race import historical_MVP_race_table
@@ -12,14 +10,13 @@ def pre_score_table(year):
         reg_table = reg_stats_table(year)
     except ValueError as e:
         raise ValueError (f'Error while scrapping data for season {year}: {e}')
-    
     #merging the advanced_stats and reg_stats table so that we can use all the stats in the weighted equation to calculated MVP score
     #we are only merging players with the same name, age, and team in the same season. This is to prevent merging wrong stats in the event
     #that players share the same name. The likelihood of players sharing the same name, age, and team in the same season is very very low.
-    #players are dropped if they appear on one table, but not the other. This is because reg_stats manually calculated total minutes played, resulting 
+    #players are dropped if they appear on one table, but not the other. This is because reg_stats manually calculated total minutes played, resulting
     #in fringe cases where they may have met the 1560 mark in the advanced_table, but not for the reg_table as we used averaged MPG. These players that
     #barely made the mark are insigifcant to our mvp candidates shortlist.
-   
+    
     merged_df = pd.merge(advanced_table, reg_table, on=['name', 'age', 'team'], how= "inner")
     return merged_df
 
@@ -45,9 +42,9 @@ def weightage():
         for stat_norm in default_weights.items():
             while True:
                 try:
-                    weight = float(input(f"Enter weight for {stat_norm} (0 to 1): "))
-                    if 0 <= weight <= 1:
-                        weights[stat_norm] = weight
+                    value = float(input(f"Enter weight for {stat_norm} (0 to 1): "))
+                    if 0 <= value <= 1:
+                        weights[stat_norm] = value
                         break
                     else:
                         print("Please enter a number between 0 and 1.")
@@ -64,33 +61,17 @@ def weightage():
 
 def full_table(df, weights):
     stats = ['PER', 'TS', 'WS', 'BPM', 'PPG', 'AST', 'TRB', 'BLK', 'STL']
-    #create a new column for every normalised stats for every player. min-max normalisation used
     for stat in stats:
+        #make a new column for every stat, but normalised. min-max normalisation is used
         df[f'{stat}_norm'] = (df[stat] - df[stat].min()) / (df[stat].max() - df[stat].min())
 
-    #create a new column called 'MVP_score', which is found by getting (normalised stat x of a player * assigned weight) for every stat, then adding them together 
+    #to calculated MVP score, we used (nomalised stat x * weightage for that stat). We do that for every stat of a player and add them together
     df['MVP_score'] = sum(df[stat_norm] * weight for stat_norm, weight in weights.items())
     df = df.sort_values('MVP_score', ascending= False)
 
-    new_column_order = ['name', 'age', 'team', 'MVP_score', 'PER' ,'TS', 'WS', 'BPM', 'PPG', 'AST',
-                        'TRB', 'BLK', 'STL', 'PER_norm','TS_norm', 'WS_norm', 'BPM_norm', 'PPG_norm', 'AST_norm',
-                        'TRB_norm', 'BLK_norm', 'STL_norm', 'minutes_played']
-    df_reordered = df[new_column_order]
+    cols = list(df.columns)
+    mins_played_col_index, mvp_score_col_index = cols.index('minutes_played'), cols.index('MVP_score')
+    cols[mvp_score_col_index], cols[min_played_index] = cols[mins_played_index], cols[mvp_score_col_index]
+    df = df[cols]
 
-    return df_reordered.head(10)
-
-
-
-
-
-what_year = int(input("Which season's full stats would you like?: "))
-
-try:
-    merged_df = pre_score_table(what_year)
-    weights = weightage()
-    print(' ')
-    print(full_table(merged_df, weights).to_string())
-    print(' ')
-    print(historical_MVP_race_table(what_year).to_string())
-except ValueError as e:
-    print(f'Error: {e}')
+    return df.head(10)
